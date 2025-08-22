71e0474f145ded88b022e20b63cdc702bd04c21b
filip.chytil <filip.chytil@firma.seznam.cz>
2025-08-22 14:44:00 +0200

    added presave

--- tinygent/datamodels/llm.py
+++ tinygent/datamodels/llm.py
@@ -1,5 +1,57 @@
+import typing
+
 from abc import ABC
+from abc import abstractmethod
+from langchain_core.outputs import LLMResult
+from pydantic import BaseModel
+from typing import Generic
+
+if typing.TYPE_CHECKING:
+    from langchain_core.prompt_values import PromptValue
+
+LLMConfigT = typing.TypeVar('LLMConfigT', bound=BaseModel)
+LLMStructuredT = typing.TypeVar('LLMStructuredT', bound=BaseModel)
 
 
-class AbstractLLM(ABC):
-    pass
+class AbstractLLM(ABC, Generic[LLMConfigT]):
+
+    @abstractmethod
+    def __init__(
+        self,
+        config: LLMConfigT,
+        *args,
+        **kwargs
+    ) -> None: ...
+
+    @property
+    @abstractmethod
+    def config(self) -> LLMConfigT: ...
+
+    @property
+    def supports_tool_calls(self) -> bool: ...
+
+    @abstractmethod
+    def generate_text(
+        self,
+        prompt: PromptValue
+    ) -> LLMResult: ...
+
+    @abstractmethod
+    async def agenerate_text(
+        self,
+        prompt: PromptValue
+    ) -> LLMResult: ...
+
+    @abstractmethod
+    def generate_structured(
+        self,
+        prompt: PromptValue,
+        output_schema: LLMStructuredT
+    ) -> LLMStructuredT: ...
+
+    @abstractmethod
+    async def agenerate_structured(
+        self,
+        prompt: PromptValue,
+        output_schema: LLMStructuredT
+    ) -> LLMStructuredT: ...
