e778bc83d8b8b1f1b5b1f40c85620721440884e5
filip.chytil <filip.chytil@firma.seznam.cz>
2025-09-18 18:47:12 +0200

    added

--- tinygent/utils/answer_validation.py
+++ tinygent/utils/answer_validation.py
@@ -1,6 +1,6 @@
-from tinygent.datamodels.messages import TinyAIMessage
+from tinygent.datamodels.messages import BaseMessage
 
 
-def is_final_answer(message: type[TinyAIMessage]) -> bool:
+def is_final_answer(message: BaseMessage) -> bool:
     """Check if the message is marked as the final answer."""
     return message.metadata.get('is_final_answer', False)
