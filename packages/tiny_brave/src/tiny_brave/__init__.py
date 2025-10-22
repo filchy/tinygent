821709504367c5419810b5b870f3e68ef584fa77
filip.chytil <filip.chytil@firma.seznam.cz>
2025-10-23 00:33:33 +0200

    add

--- packages/tiny_brave/src/tiny_brave/__init__.py
+++ packages/tiny_brave/src/tiny_brave/__init__.py
@@ -7,13 +7,13 @@ from tiny_brave.responses.news import NewsSearchApiResponse
 from tiny_brave.tools import brave_news_search
 from tiny_brave.tools import brave_web_search
 
-__ALL__ = [
-    TinyBraveClient,
-    TinyBraveError,
-    TinyBraveClientError,
-    TinyBraveAPIError,
-    NewsSearchApiResponse,
-    NewsSearchRequest,
-    brave_news_search,
-    brave_web_search
+__all__ = [
+    'TinyBraveClient',
+    'TinyBraveError',
+    'TinyBraveClientError',
+    'TinyBraveAPIError',
+    'NewsSearchApiResponse',
+    'NewsSearchRequest',
+    'brave_news_search',
+    'brave_web_search',
 ]
