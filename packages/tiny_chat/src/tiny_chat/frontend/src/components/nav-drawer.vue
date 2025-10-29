0242142acadad77c7cdd5292fe36cd4b4103b8d7
filip.chytil <filip.chytil@firma.seznam.cz>
2025-10-29 21:51:48 +0100

    added

--- packages/tiny_chat/src/tiny_chat/frontend/src/components/nav-drawer.vue
+++ packages/tiny_chat/src/tiny_chat/frontend/src/components/nav-drawer.vue
@@ -1,5 +1,4 @@
 <script setup lang='ts'>
-import lightAvatar from '@/assets/light-avatar.png'
 import darkAvatar from '@/assets/dark-avatar.png'
 
 const props = defineProps<{
@@ -21,38 +20,37 @@ const rail = ref(false)
     permanent
     app
   >
-    <v-list class='pa-0'>
-      <v-list-item>
-        <template #prepend>
-          <v-img
-            :src="darkAvatar"
-            width="28"
-            height="64"
-            class="mr-2"
-            cover
-          />
-        </template>
+    <div class='d-flex align-center justify-space-between px-1 py-2' style='height: 64px;'>
+      <v-img
+        :src='darkAvatar'
+        max-width='56'
+        max-height='56'
+        class='rounded transition-fast-in-fast-out'
+        contain
+        :style='{ opacity: rail ? 0 : 1, visibility: rail ? "hidden" : "visible" }'
+      />
 
-        <template #append>
-          <v-btn
-            icon
-            variant='text'
-            @click.stop='rail = !rail'
-          >
-            <v-icon>{{ rail ? 'mdi-chevron-right' : 'mdi-chevron-left' }}</v-icon>
-          </v-btn>
-        </template>
-      </v-list-item>
-    </v-list>
+      <v-btn
+        icon
+        variant='text'
+        size='small'
+        @click.stop='rail = !rail'
+      >
+        <v-icon>{{ rail ? 'mdi-chevron-right' : 'mdi-chevron-left' }}</v-icon>
+      </v-btn>
+    </div>
 
-    <v-divider></v-divider>
-
-    <v-list nav dense>
-      <v-list-subheader v-show='!rail'>Conversations</v-list-subheader>
+    <v-list
+      nav
+      dense
+      class='transition-fast-in-fast-out'
+      :style='{ opacity: rail ? 0 : 1, visibility: rail ? "hidden" : "visible" }'
+    >
+      <v-list-subheader>Conversations</v-list-subheader>
       <v-list-item
         v-for='(conv, i) in props.conversations || []'
         :key='i'
-        :title='!rail ? conv : ""'
+        :title='conv'
         prepend-icon='mdi-message-text'
       />
     </v-list>
