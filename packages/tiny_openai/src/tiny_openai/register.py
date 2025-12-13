314c5145dab8f69cda8a9bce870da595ade781e4
filip.chytil <filip.chytil@firma.seznam.cz>
2025-12-13 15:56:36 +0100

    added init commit with renamed config models
    renamed config models of llms to contain LLM keyword, so that packages
    are prepared for embedders

--- packages/tiny_openai/src/tiny_openai/register.py
+++ packages/tiny_openai/src/tiny_openai/register.py
@@ -1,11 +1,11 @@
-from tiny_openai.embedder import OpenAIEmbedder
-from tiny_openai.embedder import OpenAIEmbedderConfig
-from tiny_openai.llm import OpenAILLM
-from tiny_openai.llm import OpenAILLMConfig
-from tinygent.runtime.global_registry import GlobalRegistry
-
-
 def _register_openai() -> None:
+    from tinygent.runtime.global_registry import GlobalRegistry
+
+    from .embedder import OpenAIEmbedder
+    from .embedder import OpenAIEmbedderConfig
+    from .llm import OpenAILLM
+    from .llm import OpenAILLMConfig
+
     registry = GlobalRegistry().get_registry()
 
     registry.register_llm('openai', OpenAILLMConfig, OpenAILLM)
